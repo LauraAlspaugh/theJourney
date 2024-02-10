8860e3ffd7eba96333e45e03aa1b159a4c1ffbf6
Laura <lauraalspaugh2002@icloud.com>
2024-02-10 12:31:47 -0700

    first commit

---
+++ server/Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using theJourney.Repositories;
+global using theJourney.Services;
+global using theJourney.Models;
